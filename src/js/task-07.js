f60d8cd11710a6735979dead5fb56122373ed4f7
Konurbaeva <konurbaevamadina@gmail.com>
2022-01-15 20:57:09 +0100

    removed console.log from task 7

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -2,7 +2,6 @@ const inputEl = document.getElementById('font-size-control');
 const spanEl = document.getElementById('text');
 
 const onInputChange = function (event){
-    console.log(event.currentTarget.value);
     spanEl.style.fontSize = `${event.currentTarget.value}px`
  };
   
