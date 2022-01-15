b1a8263c39254d65d51d6dc868b81ef07796098c
Konurbaeva <konurbaevamadina@gmail.com>
2022-01-15 22:28:56 +0100

    added const currentColor = `${getRandomHexColor()}`; task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
+const buttonEl = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+
+const currentColor = `${getRandomHexColor()}`;
+
+console.log("currentColor: " + currentColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changesBackgroundColorOfBody = function() {
+
+  document.body.style.backgroundColor = currentColor;
+  spanRef.textContent = currentColor;
+}
+
+buttonEl.addEventListener('click', changesBackgroundColorOfBody);
