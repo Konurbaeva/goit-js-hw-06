bcaebae162b6cdfb545f3e6408515f60f2fb7927
Konurbaeva <konurbaevamadina@gmail.com>
2022-01-15 16:57:22 +0100

    All data-XXX attributes are turned into properties of the dataset property. So we should use destructuring

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,13 +1,17 @@
+const counterValue = 0;
+const button = document.querySelector('button');
 const decrement = document.querySelector("[data-action='decrement']");
 const increment = document.querySelector("[data-action='increment']");
 
-const handleDecrementClick = () => {
-  console.log("decrement clicked");
+const handleClick = function (event){
+  const { dataset: { action: name }, value } = event.target;
+  
+  console.log(name)
+  console.log(value)
+   
 };
 
-const handleIncrementClick = () => {
-    console.log("increment clicked");
-  };
+decrement.addEventListener("click", handleClick);
+increment.addEventListener("click", handleClick);
 
-decrement.addEventListener("click", handleDecrementClick);
-increment.addEventListener("click", handleIncrementClick);
+
