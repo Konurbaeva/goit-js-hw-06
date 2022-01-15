802f597bc433c3fc845440b2409903815d28f493
Konurbaeva <konurbaevamadina@gmail.com>
2022-01-15 17:54:17 +0100

    added counter and fixed task 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,14 +1,22 @@
-const counterValue = 0;
-const button = document.querySelector('button');
+let counterValue = 0;
+let counter = document.getElementById('value');
 const decrement = document.querySelector("[data-action='decrement']");
 const increment = document.querySelector("[data-action='increment']");
 
+
 const handleClick = function (event){
-  const { dataset: { action: name }, value } = event.target;
+  const { dataset: { action: name }} = event.target;
   
-  console.log(name)
-  console.log(value)
-   
+  if(name === 'increment'){
+    const num = parseInt(counter.innerHTML);
+    counter.innerHTML = num + 1;
+    counterValue = counter;
+
+  } else if (name === 'decrement'){
+    const num = parseInt(counter.innerHTML);
+    counter.innerHTML = num - 1;
+    counterValue = counter;
+  }
 };
 
 decrement.addEventListener("click", handleClick);
