e950c1c09a47331788b9db89df50033d6ba00c25
Konurbaeva <konurbaevamadina@gmail.com>
2022-01-16 08:50:37 +0100

    шаблонная строка тут не нужна, + px

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,8 +1,10 @@
 const inputEl = document.getElementById('font-size-control');
 const spanEl = document.getElementById('text');
 
+
+
 const onInputChange = function (event){
-    spanEl.style.fontSize = `${event.currentTarget.value}px`
+    spanEl.style.fontSize = event.target.value + 'px'
  };
   
 inputEl.addEventListener('input', onInputChange);
