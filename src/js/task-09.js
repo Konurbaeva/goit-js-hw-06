ee4b2f7b46ac44b305bb757f8de46e1bfd728bfa
Konurbaeva <konurbaevamadina@gmail.com>
2022-01-16 10:49:28 +0100

    fixed task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,16 +1,13 @@
 const buttonEl = document.querySelector('.change-color');
 const spanRef = document.querySelector('.color');
 
-const currentColor = `${getRandomHexColor()}`;
-
-console.log("currentColor: " + currentColor);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 const changesBackgroundColorOfBody = function() {
-
+  const currentColor = `${getRandomHexColor()}`;
   document.body.style.backgroundColor = currentColor;
   spanRef.textContent = currentColor;
 }
